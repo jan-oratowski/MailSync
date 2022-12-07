5147c48baea002131c8944b22b8864fd30056661
Jan Oratowski <54535207+jan-oratowski@users.noreply.github.com>
2022-12-07 16:36:58 +0100

    refactoring / rules in separate class

---
+++ MailSync.Core/Models/FolderRule.cs
@@ -0,0 +1,10 @@
+﻿namespace MailSync.Core.Models
+{
+    public class FolderRule
+    {
+        public int Id { get; set; }
+        public User User { get; set; } = null!;
+        public List<Folder> Folders { get; set; } = new();
+        public Folder? Target { get; set; }
+    }
+}
